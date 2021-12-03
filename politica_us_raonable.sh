14e85b45fd8773e8bf732c0f7aff67bbf600aa92
Oriol Lorenzo <oriol.lorenzo@urv.cat>
2021-12-03 08:59:02 +0100

    Update politica_us_raonable.sh

--- politica_us_raonable.sh
+++ politica_us_raonable.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
+# please, adapt it to your system 
 
-source /home/oriol/.profile
+
+source /home/user/.profile
 
 cd /opt/PUR && /usr/bin/python3.9 -W ignore politica_us_raonable.py >> logg.out
